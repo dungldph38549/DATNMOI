096d05f6b54792fef311d3de589379e91ad3e1af
nguyenthethinh <thinhntph38327@fpt.edu.vn>
2026-02-28 16:49:55 +0700

    update code

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,23 +2,34 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./redux/counterSlice";
 import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ShoppingCartPage from "./pages/ShoppingCartPage/ShoppingCartPage";
+import TermsConditionsPage from "./pages/TermsConditionsPage/TermsConditionsPage";
+import HomePage from "./pages/HomePage/HomePage";
+
+
+const Button = styled.button`
+  color: grey;
+`;
+
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-  const Button = styled.button`
-    color: grey;
-  `;
 
   return (
-    <div>
-      <div>
-        <Button onClick={() => dispatch(increment())}>Increment</Button>
+    <BrowserRouter>
+      <Routes>
 
-        <span>{count}</span>
+        <Route path="/" element={<HomePage />} />
+        
+        <Route path="/cart" element={<ShoppingCartPage />} />
 
-        <Button onClick={() => dispatch(decrement())}>Decrement</Button>
-      </div>
-    </div>
+        <Route path="/terms" element={<TermsConditionsPage />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
+export default Counter;
--- FE/src/index.js
+++ FE/src/index.js
@@ -7,6 +7,20 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import "antd/dist/antd.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/font-awesome.css";
+import "./assets/css/animate.min.css";
+import "./assets/css/owl.carousel.css";
+import "./assets/css/owl.transitions.css";
+import "./assets/css/bootstrap-select.min.css";
+import "./assets/css/lightbox.css";
+import "./assets/css/rateit.css";
+import "./assets/css/blue.css";
+import "./assets/css/main.css";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
